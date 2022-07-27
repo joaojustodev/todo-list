ff0f8f3a96160079806fcae31d0791861cdbc7a3
joaobr200 <joaoarantesjob@gmail.com>
2022-07-27 17:33:38 -0300

    feat: add a secutiry popover before remove todo

--- src/components/Home/TodoList/index.tsx
+++ src/components/Home/TodoList/index.tsx
@@ -1,16 +1,39 @@
+import * as Popover from "@radix-ui/react-popover";
 import { useTodos } from "../../../hooks/useTodos";
-import styles from "./todolist.module.scss";
-import { Check, Trash } from "phosphor-react";
 import { useUpdateTodo } from "../../../hooks/useUpdateTodo";
 import { useDeleteTodo } from "../../../hooks/useDeleteTodo";
+import styles from "./todolist.module.scss";
+import { Check, Trash } from "phosphor-react";
+import useLocalStorage from "../../../hooks/useLocalStorage";
+import { useState } from "react";
 
 const TodoList = () => {
   const { data, isLoading } = useTodos();
   const { mutate: updateTodo } = useUpdateTodo();
   const { mutate: deteleTodo } = useDeleteTodo();
 
+  // NOTE: VERIFICA SE O POPOVER JA FOI MARCADO COMO TRUE, ASSIM ELE NAO APARECE MAIS A MENSAGEM DE SEGURANÇA
+  const [hideTrashMessageStorage, setHideTrashMessageStorage] = useLocalStorage(
+    "hide-trash-message",
+    false
+  );
+
+  //NOTE: STATE DO CHECKBOX DO POPOVER DA LIXEIRA
+  const [hideTrashMessageCheckBox, setHideTrashMessageCheckBox] =
+    useState<boolean>(false);
+
+  //NOTE: LOADING DOS TODOS
   if (!data || isLoading) {
     return <div>loading...</div>;
+  }
+
+  // NOTE:FUNÇÃO QUE VERIFICA QUANDO POPOVER DA LIXEIRA FOR FECHAR E SE O CHECKBOX DO POPOVER PARA ESCONDE-LO ESTÁ FECHADO
+  function hideTrashMessage(e: boolean) {
+    if (!e) {
+      if (hideTrashMessageCheckBox) {
+        setHideTrashMessageStorage(!hideTrashMessageStorage);
+      }
+    }
   }
 
   return (
@@ -36,15 +59,61 @@ const TodoList = () => {
                   </div>
                   <span>{todo.name}</span>
                 </div>
-
-                <button
-                  type="button"
-                  title="Deletar todo"
-                  onClick={() => deteleTodo({ id: todo.id })}
-                  className={styles.todolistTrashWrapper}
-                >
-                  <Trash aria-hidden size={18} />
-                </button>
+                {/** NOTE: MELHORAR ACESSIBILIDADE AQUI */}
+                {hideTrashMessageStorage ? (
+                  <button
+                    className={styles.todolistTrashWrapper}
+                    type="button"
+                    title="Remove todo"
+                    onClick={() => deteleTodo({ id: todo.id })}
+                  >
+                    <Trash aria-hidden size={18} />
+                  </button>
+                ) : (
+                  <Popover.Root onOpenChange={(e) => hideTrashMessage(e)}>
+                    <Popover.Trigger
+                      title=""
+                      aria-labelledby="Click here for remove todo"
+                      className={styles.todolistTrashWrapper}
+                    >
+                      <Trash aria-hidden size={18} />
+                    </Popover.Trigger>{" "}
+                    <Popover.Portal>
+                      <Popover.Content
+                        aria-describedby="Remove todo"
+                        className={styles.todolistTrashPopoverContent}
+                      >
+                        <Popover.Arrow aria-hidden />
+                        <div
+                          className={styles.todolistTrashPopoverContentWrapper}
+                        >
+                          <span>
+                            Tem certeza que deseja excluir esse todo ?
+                          </span>
+                          <Popover.Close
+                            type="button"
+                            title="Remove todo"
+                            onClick={() => deteleTodo({ id: todo.id })}
+                          >
+                            Excluir
+                          </Popover.Close>
+                        </div>
+                        <div
+                          className={styles.todolistTrashPopoverContentCheckBox}
+                        >
+                          <input
+                            type="checkbox"
+                            checked={hideTrashMessageCheckBox}
+                            onChange={() =>
+                              setHideTrashMessageCheckBox((old) => !old)
+                            }
+                          />{" "}
+                          <span>Não aparecer mais essa mensagem</span>
+                        </div>
+                      </Popover.Content>
+                    </Popover.Portal>
+                  </Popover.Root>
+                )}
               </li>
             ))}
           </ul>
