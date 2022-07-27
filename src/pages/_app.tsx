b63bcfa98885bb142a1b47df851b345e39cbc4e5
joaobr200 <joaoarantesjob@gmail.com>
2022-07-27 16:09:42 -0300

    add: react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { query } from "../lib/query";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={query}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
