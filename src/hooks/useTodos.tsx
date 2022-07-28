dbe2e4c64eb7a8c154c00fe69338bb845a22a5ba
joaobr200 <joaoarantesjob@gmail.com>
2022-07-28 16:28:52 -0300

    api: add log error

--- src/hooks/useTodos.tsx
+++ src/hooks/useTodos.tsx
@@ -3,9 +3,17 @@ import { api } from "../lib/api";
 import { TodoRepositorie } from "../repositories/todoRepositorie";
 
 export const useTodos = () => {
-  return useQuery(["todos"], async () => {
-    const { data } = await api.get<TodoRepositorie[]>("/api/todo/list");
+  return useQuery(
+    ["todos"],
+    async () => {
+      const { data } = await api.get<TodoRepositorie[]>("/api/todo/list");
 
-    return data;
-  });
+      return data;
+    },
+    {
+      onError: (error) => {
+        console.log(error);
+      },
+    }
+  );
 };
