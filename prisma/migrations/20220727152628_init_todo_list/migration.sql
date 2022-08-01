db0de54e7f8a76f7ddeaf083a4cba37409458610
joaobr200 <joaoarantesjob@gmail.com>
2022-08-01 17:30:08 -0300

    prisma config

--- prisma/migrations/20220727152628_init_todo_list/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "slug" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "isDone" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
