644e8f8a6c289677b8d775800f60a7abca706d3e
joaobr200 <joaoarantesjob@gmail.com>
2022-08-01 18:08:24 -0300

    types: next auth

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,19 @@
+import { JWT } from "next-auth/jwt";
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    accessToken: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    accessToken?: string;
+  }
+}
