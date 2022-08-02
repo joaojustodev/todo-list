38f13939bff29f997b8190ddde80fca28e306b28
joaobr200 <joaoarantesjob@gmail.com>
2022-08-02 15:34:14 -0300

    fix middleware redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,16 @@
 // middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-const SESSION_TOKEN_COOKIE = "next-auth.session-token";
+import { SESSION_TOKEN_COOKIE } from "./constants";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
   const sessionToken = req.cookies.get(SESSION_TOKEN_COOKIE);
-
   if (!sessionToken) {
+    if (req.nextUrl.pathname.startsWith("/tasks")) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
     return NextResponse.error();
   }
 
