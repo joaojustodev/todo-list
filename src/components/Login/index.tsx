cdd17626fce9a6eb423cb5802b0016e1c92df8ba
joaobr200 <joaoarantesjob@gmail.com>
2022-08-02 18:45:10 -0300

    feat: login page

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,57 @@
+import { signIn } from "next-auth/react";
+import Logo from "../Ui/Logo";
+import styles from "./login.module.scss";
+import { FacebookLogo, GithubLogo, GoogleLogo } from "phosphor-react";
+
+const Login = () => {
+  return (
+    <section className={styles.loginBlock}>
+      <div className={styles.loginLeftSide}>
+        <Logo size={56} />
+      </div>
+
+      <div className={styles.loginRightSide}>
+        <div className={styles.loginLogoWrapper}>
+          <Logo size={56} />
+        </div>
+        <div className={styles.loginWrapper}>
+          <h2>Connect with</h2>
+
+          <div className={styles.loginSocialWrapper}>
+            <button
+              type="button"
+              title="Connect with github account"
+              className={styles.loginSocialGithub}
+              onClick={() => signIn("github")}
+            >
+              <GithubLogo aria-hidden />
+              <span>Github</span>
+            </button>
+            <button
+              type="button"
+              title="Connect with facebook account"
+              disabled
+              aria-disabled
+              className={styles.loginSocialFacebook}
+            >
+              <FacebookLogo />
+              Facebook
+            </button>
+            <button
+              type="button"
+              title="Connect with google account"
+              disabled
+              aria-disabled
+              className={styles.loginSocialGoogle}
+            >
+              <GoogleLogo />
+              Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,12 @@
 import { GetServerSideProps } from "next";
-import { signIn, useSession } from "next-auth/react";
 import { unstable_getServerSession } from "next-auth/next";
 import { nextAuthOptions } from "./api/auth/[...nextauth]";
+import Login from "../components/Login";
 
 const Home = () => {
-  const { data, status } = useSession({
-    required: true,
-    onUnauthenticated: () => alert("Voce não foi autorizado"),
-  });
-  console.log("DATA:", data);
-  console.log("STATUS:", status);
-
   return (
-    <main
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div>
-        <button type="button" onClick={() => signIn("github")}>
-          Fazer Login com github
-        </button>
-      </div>
+    <main>
+      <Login />
     </main>
   );
 };
