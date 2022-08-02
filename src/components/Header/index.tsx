02aaa9f6d64e35af016d4f564411267d2f641151
joaobr200 <joaoarantesjob@gmail.com>
2022-08-02 18:44:59 -0300

    feat: logo ui componenet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Avatar from "../Ui/Avatar";
 import type { Session } from "next-auth";
-import { CheckSquare } from "phosphor-react";
 import styles from "./header.module.scss";
+import Logo from "../Ui/Logo";
 
 interface HeaderProps {
   session: Session;
@@ -12,17 +12,7 @@ const Header = ({ session }: HeaderProps) => {
     <header className={styles.header}>
       <div className="containerLg">
         <nav className={styles.headerNav}>
-          <div className={styles.headerTitleWrapper}>
-            <h1 className={styles.headerTitle}>
-              TodoList
-              <span>
-                <CheckSquare aria-hidden size={28} />
-              </span>
-            </h1>
-
-            <div className={styles.headerTitleRedBlock}></div>
-            <div className={styles.headerTitleGreenBlock}></div>
-          </div>
+          <Logo size={28} />
 
           <Avatar src={session.user?.image as string} />
         </nav>
