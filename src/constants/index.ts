179c48f6e1e7fb072ffe4444d75e18cb3d452b80
joaobr200 <joaoarantesjob@gmail.com>
2022-08-03 02:21:00 -0300

    fix: constant session cookie name to preview

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const SESSION_TOKEN_COOKIE =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV !== "development"
     ? "__Secure-next-auth.session-token"
     : "next-auth.session-token";
