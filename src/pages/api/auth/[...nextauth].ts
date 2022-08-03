d4baa2226fd2d455bfd7cac7868c0258276b95eb
joaobr200 <joaoarantesjob@gmail.com>
2022-08-03 15:00:00 -0300

    add logout params to signout

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -24,7 +24,7 @@ export const nextAuthOptions: NextAuthOptions = {
   pages: {
     signIn: "/",
     error: "/",
-    signOut: "/",
+    signOut: "/?logout=true",
   },
   events: {
     async linkAccount(message) {
