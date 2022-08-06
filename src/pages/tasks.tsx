8286831895ab230489d906a6af626a3123b5e78c
joaobr200 <joaoarantesjob@gmail.com>
2022-08-06 17:43:20 -0300

    feat: SEO

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -14,11 +14,6 @@ interface TasksProps {
 const Tasks = ({ session }: TasksProps) => {
   return (
     <>
-      <Head>
-        <title>TodoList - joaojustodev</title>
-        <meta name="description" content="Todo list with NextJS" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
       <Header session={session} />
       <main>
         <section>
